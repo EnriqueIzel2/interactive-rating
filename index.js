1187086d12df835e98e20699fc3d495138231749
EnriqueIzel2 <eleaoizel@gmail.com>
2022-06-06 18:40:40 -0400

    feat: add functions to change card

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
-const $ratingItems = document.querySelector(".rating-container");
+const $ratingItems = document.querySelectorAll(".rating-item");
+const $btnSubmit = document.querySelector(".button-submit");
+const $ratingCard = document.querySelector("#rating-card");
+const $thanksCard = document.querySelector("#thanks-card");
+const $resultRating = document.querySelector("#result-rating");
 
-$ratingItems.addEventListener("click", function (e) {
+let ratingValue;
+
+$ratingItems.forEach(($button) =>
+  $button.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    removeSelectedfromButtons($ratingItems);
+
+    ratingValue = e.target.textContent;
+    e.target.classList.add("rating-item__selected");
+  })
+);
+
+function removeSelectedfromButtons($buttons) {
+  $buttons.forEach(($button) => {
+    $button.classList.remove("rating-item__selected");
+  });
+}
+
+$btnSubmit.addEventListener("click", function (e) {
   e.preventDefault();
-  console.log("Funão select:", $ratingItems);
+
+  $resultRating.textContent = ratingValue;
+  activeNewCard($ratingCard, $thanksCard);
 });
+
+function activeNewCard($prevCard, $nextCard) {
+  $prevCard.classList.add("deactive");
+  $nextCard.classList.remove("deactive");
+}
